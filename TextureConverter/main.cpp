e3a20b12a10927d0405f14cbd313a01f4b596535
YuukiSakihiro <k021g1259@g.neec.ac.jp>
2023-10-03 15:01:35 +0900

    2-3

--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -1,8 +1,13 @@
-#include<cstdio>
-#include<cstdlib>
-int main()
+#include"stdio.h"
+#include"stdlib.h"
+int main(int argc,char* argv[])
 {
-	printf("Hello,World\n");
+	for (int i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+		printf("\n");
+	}
+	
 	system("pause");
 	return 0;
 }
